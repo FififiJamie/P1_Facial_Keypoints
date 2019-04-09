5455ad4ad814b536ade4e095f646816ba0c79e1f
Jamie <jamie.zhang@bbctechnologies.com>
2019-04-10 09:05:13 +1200

    feat: use batchnorm and uniform-init weights, also improve the way to calculate accuracy (MSE)

--- models.py
+++ models.py
@@ -23,23 +23,32 @@ class Net(nn.Module):
         
         self.conv1 = nn.Conv2d(1, 32, 5) # 220 * 220
         
+        self.conv1_bn = nn.BatchNorm2d(32)
+        
         # max pool 2 * 2 --> 110 * 110
         
         self.conv2 = nn.Conv2d(32, 64, 3) # 108 * 108
         
-        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2_bn = nn.BatchNorm2d(64)
         
+        self.pool = nn.MaxPool2d(2, 2)
+       
         # max pool 2 * 2  --> 54 * 54
         
         self.conv3 = nn.Conv2d(64, 128, 3) # 52 * 52
+        self.conv3_bn = nn.BatchNorm2d(128)
         
         # max pool 2 * 2  --> 26 * 26
         
         self.conv4 = nn.Conv2d(128, 256, 3) # 24 * 24
+        self.conv4_bn = nn.BatchNorm2d(256)
         
         # max pool 2 * 2  --> 12 * 12
         
         self.conv5 = nn.Conv2d(256, 512, 1) # 12 * 12
+        self.conv5_bn = nn.BatchNorm2d(512)
+        
+        # max pool 2 * 2  --> 6 * 6
         
         self.dropout = nn.Dropout(0.5)
         
@@ -54,16 +63,16 @@ class Net(nn.Module):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv1(x))) #
-        x = self.dropout(x)
-        x = self.pool(F.relu(self.conv2(x))) # 
-        x = self.dropout(x)
-        x = self.pool(F.relu(self.conv3(x))) # 
-        x = self.dropout(x)
-        x = self.pool(F.relu(self.conv4(x))) # 
-        x = self.dropout(x)
-        x = self.pool(F.relu(self.conv5(x))) # 
-        x = self.dropout(x)
+        x = self.pool(F.relu(self.conv1_bn(self.conv1(x)))) #
+        # x = self.dropout(x)
+        x = self.pool(F.relu(self.conv2_bn(self.conv2(x)))) # 
+        # x = self.dropout(x)
+        x = self.pool(F.relu(self.conv3_bn(self.conv3(x)))) # 
+        # x = self.dropout(x)
+        x = self.pool(F.relu(self.conv4_bn(self.conv4(x)))) # 
+        # x = self.dropout(x)
+        x = self.pool(F.relu(self.conv5_bn(self.conv5(x)))) # 
+        # x = self.dropout(x)
         
         x = F.relu(self.fc1(x.view(x.size(0), -1)))
         x = self.dropout(x)
